0c8789c837ffd212270ea4a0d76168b5631eb020
ysw <ysw@daou.co.kr>
2024-04-29 14:53:29 +0900

    1. 과제 2024년 4월 27일 남광호님 PR 1

--- src/smtpSession.c
+++ src/smtpSession.c
@@ -1,88 +1,69 @@
-#include <semaphore.h>
 #include "main.h"
-#include "list_func.h"
 
 /*  TODO 과제 1 공통 사항
  *  session 관리는 Socket정보를 보관하기 위해 사용합니다.
  *  다량의 smtp session 정보를 보관하는 보관소가 존재하여야 합니다..
  *  smtp session 관리 시 충돌을 방지하기 위한 적절한 매커니즘을 적용하여 개발하여야 합니다.
-*/
+ */
 
-list_t smtp_sessions[SESSION_HASH_SIZE];
-int smtp_sessions_cnt;
-sem_t g_sem;
+typedef struct _Node{
 
-static inline int getHashIndex(char *session_id)
-{
-	int i, len, tmp = 0;
+	smtp_session_t 	*pSession;
+	struct 	_Node	*pNext;
 
-	if (10 > (len = strlen(session_id))) {
-		return 0;
+}Node;
+
+#define HTBL_BUCKET_SIZE 2048
+Node	g_htbl[HTBL_BUCKET_SIZE];
+pthread_mutex_t g_mtx = PTHREAD_MUTEX_INITIALIZER;
+
+unsigned long hash_djb2(unsigned char* _p) {
+
+	unsigned long hash = 5381;
+	int c, i = 0;
+	const unsigned char* p = _p;
+	int len = strlen(p);
+
+	while( i < len )
+	{
+		c = *p++;
+		hash = ((hash << 5) + hash) + c;
+		i++;
 	}
 
-	/* 앞 4글자 공통 문자열이므로 idx 4부터 시작 */
-	for (i = 4; i < len; i++) {
-		tmp += session_id[i];
-	}
-
-	return tmp % SESSION_HASH_SIZE;
+	return hash % HTBL_BUCKET_SIZE;
 }
 
-void initSessions(void)
-{
-	int i = 0;
-
-	for (i = 0; i < SESSION_HASH_SIZE; i++) {
-		initList(&smtp_sessions[i]);
-	}
-	smtp_sessions_cnt = 0;
-	sem_init(&g_sem, 0, 1);
-}
-
-void cleanupSessions(void)
-{
-	smtp_session_t *entry = NULL;
-	list_t *pos, *next = NULL;
-	int i = 0;
-
-	sem_wait(&g_sem);
-	for (i = 0; i < SESSION_HASH_SIZE; i++) {
-		list_for_each(pos, next, &smtp_sessions[i]) {
-			entry = list_entry(pos, smtp_session_t, chain);
-			delList(&entry->chain);
-			free(entry);
-			smtp_sessions_cnt--;
-		}
-	}
-	sem_post(&g_sem);
-	sem_destroy(&g_sem);
-}
 
 void delSmtpSession(char *session_id) {
     /*  TODO 과제 1-1
      *   smtp 세션을 종료
      *   그동안 사용되었던 session을 session_id를 이용하여 session 보관소에서 제거하는 로직을 개발하여야 합니다.
      */
-	smtp_session_t *entry = NULL;
-	list_t *pos, *next = NULL;
-	int idx = 0;
+	unsigned long id = hash_djb2(session_id);
+	if( -1 != id && id < HTBL_BUCKET_SIZE )
+	{
+		pthread_mutex_lock( &g_mtx );
 
-	idx = getHashIndex(session_id);
+		Node node = g_htbl[id];
+		do{	
+			if( NULL != node.pSession )
+			{
+				if( 0 == strcmp( session_id, node.pSession->session_id) )
+				{
+					close(node.pSession->sock_fd);
+					free(node.pSession);
+					node.pSession = NULL;
+				}
+			}
 
-	sem_wait(&g_sem);
-	list_for_each(pos, next, &smtp_sessions[idx]) {
-		entry = list_entry(pos, smtp_session_t, chain);
-		if (0 != strcmp(session_id, entry->session_id)) {
-			continue;
-		}
-		LOG(LOG_DBG, "session [%s] is deleted.", entry->session_id);
-		delList(&entry->chain);
-		close(entry->sock_fd);
-		free(entry);
-		smtp_sessions_cnt--;
-		break;
+		}while(node.pNext);
+		
+
+		pthread_mutex_unlock( &g_mtx);
 	}
-	sem_post(&g_sem);
+
+    return;
 }
 
 smtp_session_t *addSmtpSession(smtp_session_t *session) {
@@ -90,16 +71,35 @@ smtp_session_t *addSmtpSession(smtp_session_t *session) {
      *   smtp 세션을 추가
      *   전달받은 smtp session정보를 활용하여 현재 관리하고 있는 smtp session 보관소에 추가하는 로직을 개발하여야 합니다.
      */
-	int idx = 0;
+	if( NULL == session )
+		return NULL;
+	
+	unsigned long id = hash_djb2(session->session_id);
+	if( -1 != id && id < HTBL_BUCKET_SIZE )
+	{
+		pthread_mutex_lock( &g_mtx );
 
-	idx = getHashIndex(session->session_id);
-	sem_wait(&g_sem);
-	addList(&session->chain, &smtp_sessions[idx]);
-	LOG(LOG_DBG, "session [%s] is added.", session->session_id);
-	smtp_sessions_cnt++;
-	sem_post(&g_sem);
+		Node node = g_htbl[id];
+		if( NULL == node.pSession )
+		{
+			node.pSession = session;
+			pthread_mutex_unlock( &g_mtx );
+			return session;
+		}
+		else
+		{// hash collision.
+			Node* p = malloc(sizeof(Node));
+			p->pSession = session;
+			p->pNext = NULL;
+			node.pNext = p;
+			pthread_mutex_unlock( &g_mtx );
+			return session;
+		}
 
-	return session;
+		pthread_mutex_unlock( &g_mtx);
+	}
+
+    return NULL;
 }
 
 smtp_session_t *getSmtpSession(char *session_id) {
@@ -107,45 +107,5 @@ smtp_session_t *getSmtpSession(char *session_id) {
      *   smtp 세션을 session_id를 활용하여 가져오려고 합니다.
      *   전달받은 smtp session_id 정보를 활용하여 현재 관리하고 있는 smtp session 보관소에서 session을 가져오는 로직을 개발하여야 합니다.
      */
-	smtp_session_t *entry = NULL;
-	list_t *pos, *next = NULL;
-	int idx = 0;
-
-	idx = getHashIndex(session_id);
-
-	sem_wait(&g_sem);
-	list_for_each(pos, next, &smtp_sessions[idx]) {
-		entry = list_entry(pos, smtp_session_t, chain);
-		if (0 != strcmp(session_id, entry->session_id)) {
-			continue;
-		}
-		LOG(LOG_DBG, "session [%s] is found.", session_id);
-		sem_post(&g_sem);
-		return entry;
-	}
-	sem_post(&g_sem);
-
-	return NULL;
-}
-
-smtp_session_t *getFirstSmtpSession(void) {
-	smtp_session_t *entry = NULL;
-	list_t *pos, *next = NULL;
-	int i = 0;
-
-	if (0 == smtp_sessions_cnt) {
-		return NULL;
-	}
-
-	sem_wait(&g_sem);
-	for (i = 0; i < SESSION_HASH_SIZE; i++) {
-		if (isEmptyList(&smtp_sessions[i])) {
-			continue;
-		}
-		sem_post(&g_sem);
-		return list_entry((&smtp_sessions[i])->next, smtp_session_t, chain);
-	}
-	sem_post(&g_sem);
-
-	return NULL;
+    return NULL;
 }
--- src/smtpSvrRecvAsync.c
+++ src/smtpSvrRecvAsync.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-int g_msqid = -1;
-
 /* TODO 과제 2 공통
  *  비동기 통신방식으로 데이터가 수신 처리되어야 합니다.
  *  기존 소스코드 변경 및 파일이 추가하는데에는 전혀 제약사향이 없습니다.
@@ -9,6 +7,80 @@ int g_msqid = -1;
  *  smtpHandleInboundConnection로직을 통해 Connection을 맺는걸 권유 드립니다.(해당 로직을 사용안하셔도 무방합니다.)
  *
  */
+#include <sys/epoll.h>
+
+
+pthread_mutex_t gMQmtx = PTHREAD_MUTEX_INITIALIZER;
+
+typedef struct _sQNode{
+
+	smtp_session_t *session;
+	struct _sQNode *next;
+
+}sQNode;
+
+sQNode gfirst; 
+
+void enqueue( smtp_session_t *session )
+{
+	pthread_mutex_lock( &gMQmtx );
+	
+	sQNode* node = calloc(1, sizeof(sQNode));
+	node->session = session;
+
+	sQNode* cur = &gfirst; 
+
+	while( 1 ){
+		if( NULL == cur->next )
+		{
+			cur->next = node;
+			break;
+		}
+		
+		cur = cur->next;
+	}
+	
+	pthread_mutex_unlock( &gMQmtx );	
+}
+
+smtp_session_t* dequeue()
+{
+	pthread_mutex_lock( &gMQmtx );
+
+	smtp_session_t *session = NULL;
+
+	sQNode* cur = &gfirst; 
+	sQNode* prev = NULL;
+
+	while( 1 )
+	{
+		if( NULL == cur->next )
+		{
+			session = cur->session;
+
+			if( prev ) 
+				prev->next = NULL;
+			if( &gfirst != cur ) 
+				free(cur);
+
+			break;
+		}
+		
+		prev = cur;
+		cur = cur->next;
+	}
+
+	pthread_mutex_unlock( &gMQmtx );	
+	
+	return session;
+}
+
+
+#define EPOLL_WAIT_TIME		(-1)
+#define MAXEVENTS		2048
+
+int epoll_fd;		// file descriptor refererring to the new epoll instance.
+
 void smtpWaitAsync(int server_fd) {
         /* TODO 과제 2-1
         *  smtpSvrRecvAsync.c 파일은 비동기 처리를 이용하여 데이터를 제어 하는 로직이 작성 되어 있습니다.
@@ -16,56 +88,66 @@ void smtpWaitAsync(int server_fd) {
         *  smtpWaitAsync Function에서 Connection을 맺고(smtpHandleInboundConnection로직 사용 권유)
         *  비동기 통신이 가능하게 개발한후 session정보를 WorkThread로 전달하는 로직을 개발하여야 합니다.
         */
+
+	int event_count;
+	struct epoll_event ie;
+	struct epoll_event *events = NULL;
 	smtp_session_t *session = NULL;
 
-	while (!g_sys_close) {
-		session = smtpHandleInboundConnection(server_fd);
-		if (NULL == session) {
-			LOG(LOG_MAJ, "ERROR: socket connection failed");
-			msleep(25);
-			continue;
-		}
+	// create epoll instance
+	epoll_fd = epoll_create1(0);
 
-		LOG (LOG_INF, "%s : %sSMTP Connection created%s : fd = %d, session_id=%s\n", __func__, C_YLLW, C_NRML,
-				session->sock_fd,
-				session->session_id);
+	// Event : server socket's client requests.
+	ie.data.fd = server_fd;
+	ie.events = EPOLLIN;
 
-		if (0 > msgsnd(g_msqid, session->session_id, strlen(session->session_id), IPC_NOWAIT)) {
-			LOG(LOG_MAJ, "ERROR: msq send failed");
-			msleep(25);
-			continue;
-		}
+	epoll_ctl( epoll_fd, EPOLL_CTL_ADD, server_fd, &ie );
 
-		msleep(25);
-	}
-}
+	events = calloc(MAXEVENTS, sizeof(struct epoll_event));
 
-void readAndDispatch(smtp_session_t *session) {
-	char buf[MAX_BUF_SIZE];
-	int nLine, nErr;
+	if( NULL == events )
+		return;
 
-	while (!g_sys_close) {
-		if ((nLine = smtpReadLine(session->sock_fd, buf, sizeof(buf))) <= 0) {
-			LOG (LOG_INF, "%s : %sSMTP Connection closed%s : fd = %d, session_id=%s\n", __func__, C_YLLW, C_NRML,
-					session->sock_fd,
-					session->session_id);
-			delSmtpSession(session->session_id);
-			break;
-		}
+	while( !g_sys_close )
+	{
+		int i = 0;
+		event_count = epoll_wait(epoll_fd, events, MAXEVENTS, EPOLL_WAIT_TIME);
+		
+		for( i; i < event_count; ++i )
+		{
+			if( events[i].data.fd == server_fd )
+			{
+				if( NULL == (session = smtpHandleInboundConnection(server_fd)))
+					break;
 
-		if ((nErr = doSmtpDispatch(session, buf)) != SMTP_DISPATCH_OK) {
-			if (nErr == SMTP_DISPATCH_FAIL) {
-				LOG(LOG_INF, "Smtp connection close by error!");
+				ie.events = EPOLLIN;
+				ie.data.fd = session->sock_fd;
+				ie.data.ptr = (void*)session;
+
+				sendGreetingMessage(session);
+				epoll_ctl( epoll_fd, EPOLL_CTL_ADD, session->sock_fd, &ie);
+				
 			}
-			delSmtpSession(session->session_id);
-			break;
-		}
+			else
+			{
+				session = events[i].data.ptr;
+				epoll_ctl( epoll_fd, EPOLL_CTL_DEL, session->sock_fd, NULL );
+
+				// session info to worker thread.
+				enqueue(session);
+			}
+		}				
 	}
+
+	free(events);
+	close(server_fd);
+	close(epoll_fd);
+
 }
 
 void *H_SERVER_WORK_TH(void *args) {
-    int nRecv;
-    char session_id[SESSION_ID_LENGTH];
+    int nLine, nErr;
+    char buf[MAX_BUF_SIZE];
     smtp_session_t *session = NULL;
 
     while (!g_sys_close) {
@@ -73,34 +155,37 @@ void *H_SERVER_WORK_TH(void *args) {
         /* TODO 과제 2-2
         *  session정보를 해당 위치에 받아 올수있게 개발하여야 합니다.
         */
+	struct epoll_event ie;
+	
+	session = dequeue();
 
-	nRecv = msgrcv(g_msqid, session_id, sizeof(session_id), 0, IPC_NOWAIT);
-	if (0 > nRecv) {
-	    if (EAGAIN != errno && ENOMSG != errno) {
-		LOG(LOG_MAJ, "ERROR: msq send failed");
-		msleep(25);
-	        continue;
-	    }
-	    /* no msg */
-            msleep(25);
-	    continue;
-	} else if (0 == nRecv) {
-	    /* no msg */
-            msleep(25);
-	    continue;
-	}
-
-	session_id[nRecv] = '\0';
-	session = getSmtpSession(session_id);
         if (session == NULL) {
-            LOG(LOG_MAJ, "ERROR: session get failed");
             msleep(25);
             continue;
         }
 
-	readAndDispatch(session);
-    }
+        if ((nLine = smtpReadLine(session->sock_fd, buf, sizeof(buf))) <= 0) {
+            LOG (LOG_INF, "%s : %sSMTP Connection closed%s : fd = %d, session_id=%s\n", __func__, C_YLLW, C_NRML,
+                 session->sock_fd,
+                 session->session_id);
+            delSmtpSession(session->session_id);
+            continue;
+        }
 
+        if ((nErr = doSmtpDispatch(session, buf)) != SMTP_DISPATCH_OK) {
+            if (nErr == SMTP_DISPATCH_FAIL) {
+                LOG(LOG_INF, "Smtp connection close by error!");
+            }
+            continue;
+        }
+
+	ie.events = EPOLLIN;
+	ie.data.fd = session->sock_fd;
+	ie.data.ptr = (void*)session;
+
+	epoll_ctl( epoll_fd, EPOLL_CTL_ADD, session->sock_fd, &ie); 
+
+    }
     return NULL;
 }
 
